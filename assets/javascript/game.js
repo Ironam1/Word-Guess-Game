097b125bf3d4f00c7d27fb6e98e2087a812d7830
Ironam1 <ironam1@icloud.com>
2019-05-06 21:44:56 -0400

    word list and selection

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,12 @@
 
+
+
+var wordList = ["wookie", "jedi", "lightsaber", "skywalker", "darth", "force", "tatooine", "blaster", "speeder", "clone",
+"droid", "ewok", "jawa", "hutt", "gungan", "death star", "imperial", "mind trick", "trooper", "crystal"]
+
+
+var compWord = wordList[Math.floor(Math.random()*wordList.length)];
+
+
+
+console.log("this is the computer's choice: " + compWord);
