810f62bfdc9fe8379351389e5aef1d87e4824575
Ironam1 <ironam1@icloud.com>
2019-05-09 10:13:33 -0400

    split word into an array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,36 +4,27 @@
 var wordList = ["wookie", "jedi", "lightsaber", "skywalker", "darth", "force", "tatooine", "blaster", "speeder", "clone",
 "droid", "ewok", "jawa", "hutt", "gungan", "death star", "imperial", "mind trick", "trooper", "crystal"]
 
-var userGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
+//var userGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
 
-var compWord;
-var wordGuess = [];
+var wordGuess = wordList[Math.floor(Math.random()*wordList.length)];
 var letterGuess = [];
 var guessLeft = 0;
 var wins = 0;
-var gameStarted = false;
 
-function resetGame () {
-    guessLeft = 0;
-    gameStarted = false;
-    compWord = Math.floor(Math.random() * (wordList.length));
-    letterGuess = [];
-    wordGuess = [];
-    for (var i = 0; i <wordList[compWord].length; i++) {
-        wordGuess.push("_");
-    }
-    updateDisplay();
-};
-
-function updateDisplay () {
-    document.getElementById("wins").innerText = "Wins: " + wins;
-    document.getElementById("comp-word").innerText = "";
-    for (var i = 0; i < compWord.length; i++) {
-        document.getElementById("comp-word").innerText += wordGuess[i];
-    }
-}
+//put the wordGuess in a box with underscore for letters
+console.log(Array.from(wordGuess));
 
 
 
+//start game and a random word is selected
+// document.onKeyPress = function() {
+//     var letter = letter.toLowerCase();
+//     for (i = 0; i < wordGuess.length; i++) {
+//         if (wordGuess.indexOf(letter) != -1);
+//         alert("letter found");
+//     }
+//}
+
+console.log("this is how many guesses left: " + guessLeft);
 console.log("this is the computer's choice: " + wordGuess);
 //console.log("this is the player's choice: " + userGuess);
