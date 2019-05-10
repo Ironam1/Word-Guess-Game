809d0955394c55f0e6d264c7aecea49a3e161ed8
Ironam1 <ironam1@icloud.com>
2019-05-10 19:59:07 -0400

    wrote some code. understand what i wrote, nothing works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,16 +18,61 @@ var winTotal = document.getElementById("wins");
 var guessTotal= document.getElementById("guesses-left");
 
 //start the game. get word, set guess left, put word in array
-function startGame () {
-         document.onkeydown = function () {
-         var selectWord = Math.floor(Math.random()* (wordList.length));
-         // var wordArr = Array.from(wordGuess);
-            for (i = 0; i < wordList[selectWord].length; i++);
-               wordGuess.push("_");
-         };
-      }
-   
+      function startGame () {   
+         document.onkeydown = function (event) {
+            var selectWord = wordList[Math.floor(Math.random()* wordList.length)];
+               for (i = 0; i < wordList[selectWord].length; i++);
+            var wordGuess = Array.from(selectWord);
+               for (j = 0; j < wordGuess.length; j++)
+               document.write(compword)(wordGuess[i] + "_");
+            var guessLeft = (wordGuess[i] + 5);
+               document.write(guessTotal)(guessLeft);
+         }
+         findLetter();
+      };
+      document.onkeydown = function(event) {
+         if (gotWord) {
+            gotWord = false;
+         } else {
+            if(event.keycode >= 65 && event.keyCode <= 90) {
+               findLetter(event.key.toLocaleLowerCase());
+            }
+         }
+      };
+      function findLetter () {
+         document.onkeydown = function (letter) {
+            var letterGuess = letter.key;
+               if (letterGuess.indexOf(letter) === -1) {
+                  letterGuess.push(letter);
+                  evaluateGuess(letter);
 
+               }
+         }
+         checkWin();
+      };
+      function evaluateGuess (letter) {
+         var positions = [];
+         for (var i = 0; i < wordList[selectWord].length; i++) {
+            if(wordList[selectWord][i] === letter) {
+               positions.push(i);
+            }
+         }
+         if (positions.length <= 0) {
+            guessLeft--;
+         } else {
+            for (var i = 0; i < positions.length; i++) {
+               wordGuess[positions[i]] = letter;
+            }
+         }
+      };
+      
+      function checkWin() {
+         if(wordGuess.indexOf("_") === -1) {
+            wins++;
+            gotWord = true;
+         }
+      };
+   
 
 
 
