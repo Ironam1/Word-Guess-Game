489f646ef07e5aac03a6d8792164e0e0c483b682
Ironam1 <ironam1@icloud.com>
2019-05-10 17:05:18 -0400

    added styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,31 +5,31 @@ var wordList = ["wookie", "jedi", "lightsaber", "skywalker", "darth", "force", "
 "droid", "ewok", "jawa", "hutt", "gungan", "death star", "imperial", "mind trick", "trooper", "crystal"]
 
 //var userGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
-
-var wordGuess = wordList[Math.floor(Math.random()*wordList.length)];
+//var wordGuess = wordList[Math.floor(Math.random) * wordList.length]
+var gotWord = false;
 var letterGuess = [];
+var selectWord;
+var wordGuess = [];
 var guessLeft = 0;
 var wins = 0;
+var compWord = document.getElementById("comp-word");
+var userGuess = document.getElementById("user-guess");
+var winTotal = document.getElementById("wins");
+var guessTotal= document.getElementById("guesses-left");
 
-//put the wordGuess in a box with underscore for letters
+//start the game. get word, set guess left, put word in array
+function startGame () {
+         document.onkeydown = function () {
+         var selectWord = Math.floor(Math.random()* (wordList.length));
+         // var wordArr = Array.from(wordGuess);
+            for (i = 0; i < wordList[selectWord].length; i++);
+               wordGuess.push("_");
+         };
+      }
+   
 
-var wordArr = Array.from(wordGuess);
-for (i = 0; i < wordArr.length; i++)
-   document.write(wordArr[i] + "_");
-
-console.log(wordArr);
 
 
 
-//start game and a random word is selected
-// document.onKeyPress = function() {
-//     var letter = letter.toLowerCase();
-//     for (i = 0; i < wordGuess.length; i++) {
-//         if (wordGuess.indexOf(letter) != -1);
-//         alert("letter found");
-//     }
-//}
 
-console.log("this is how many guesses left: " + guessLeft);
-console.log("this is the computer's choice: " + wordGuess);
-//console.log("this is the player's choice: " + userGuess);
+
